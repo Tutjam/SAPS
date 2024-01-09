9b8e4dfb7d59ddbc913126386e096e9c1166c80d
Bartosz Lech <bartosz.lech@goodsoft.pl>
2024-01-09 17:39:41 +0100

    Model subskrypcji

---
+++ app/models/Subscription.scala
@@ -0,0 +1,30 @@
+package models
+
+import play.api.libs.json.{JsObject, Json}
+
+/**
+ * Model reprezentujący subskrybenta serwisu anty-phishingowego
+ *
+ * @param userId       identyfikator
+ * @param isActive true jeżeli subskrybuje
+ */
+class Subscription(userId: Long,
+                 var isActive: Boolean) {
+  //getter
+  def getUserId: Long = userId
+
+  //setter
+  def setActivity(isActive: Boolean): Unit =
+    this.isActive = isActive
+
+  //getter
+  def getIsActive: Boolean =
+    this.isActive
+}
+
+object Subscription {
+  def toJson(subscription: Subscription): JsObject = {
+    Json.obj("userId" -> subscription.getUserId,
+      "isActive" -> subscription.isActive)
+  }
+}
